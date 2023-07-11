ccc30c3c5c0644e27b0e50f230651302b4d54f47
RogerHdzC <92351154+RogerHdzC@users.noreply.github.com>
2023-07-11 17:51:04 -0600

    TUTO ANTO

--- includes/funciones.php
+++ includes/funciones.php
@@ -6,10 +6,6 @@ define('FUNCIONES_URL', __DIR__ . 'funciones.php');
 define('CARPETA_IMAGENES', $_SERVER['DOCUMENT_ROOT'] . '/imagenes/');
 
 
-function incluirTemplate(string $nombre, bool $inicio = false){
-    include TEMPLATES_URL . "/$nombre.php";
-}
-
 function debuguear($variable) {
     echo "<pre>";
     var_dump($variable);
--- views/paginas/index.php
+++ views/paginas/index.php
@@ -1,4 +1,31 @@
 
+    <!-- WELCOME PAGE --> <!-- JOSS -->
+    <section class="welcome_page">
+        <div class="imagen">
+            <picture>
+                <source srcset="/build/img/hackpng.webp" type="image/webp">
+                <source srcset="/build/img/hackpng.png" type="image/png">
+                <img loading="lazy" src="/build/img/hackpng.png" alt="Imagen Logo">
+            </picture>
+        </div>
+    </section>
+    <!-- ABOUT --> <!-- ANTO-->
+    <section class="about">
+        <h1>ABOUT</h1>
+        <h2>A bit of history....</h2>
+        <p> The first Hack Puebla was thought in 2015 and executed in 2016. This would be its 6th edition.</p>
+        <h2>Organizers....</h2>
+        <p> Lorem ipsum</p>
+        <h2>When is the 6th edition?</h2>
+        <p> Lorem ipsum</p>
+    </section>
+    <!-- SCHEDULE --> <!-- JAZZ-->
+
+    <!-- SPONSORS --> <!-- JOSS -->
+
+    <!-- HALL OF FAME --> <!-- ANTO-->
+
+    <!-- FAQ --> <!-- JAZZ -->
     <section class="faq">
         <h2>FAQ</h2>
         <div class="contenedor contenedor-preguntas">
